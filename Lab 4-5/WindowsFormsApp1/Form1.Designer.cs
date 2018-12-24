1bf6cfd0eea9d0163e39a20b65102bbb1990e975
burlack <42722352+burlack@users.noreply.github.com>
2018-12-24 14:11:33 +0300

    Add files via upload

---
+++ Lab 4-5/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxFileReadTime = new System.Windows.Forms.TextBox();
+            this.textBoxFileReadCount = new System.Windows.Forms.TextBox();
+            this.textBoxFind = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxExactTime = new System.Windows.Forms.TextBox();
+            this.textBoxMaxDist = new System.Windows.Forms.TextBox();
+            this.textBoxThreadCount = new System.Windows.Forms.TextBox();
+            this.textBoxThreadCountAll = new System.Windows.Forms.TextBox();
+            this.textBoxApproxTime = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "*.txt)|*.txt";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(289, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Чтение файла";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxFileReadTime
+            // 
+            this.textBoxFileReadTime.BackColor = System.Drawing.SystemColors.Info;
+            this.textBoxFileReadTime.Location = new System.Drawing.Point(427, 12);
+            this.textBoxFileReadTime.Name = "textBoxFileReadTime";
+            this.textBoxFileReadTime.Size = new System.Drawing.Size(207, 38);
+            this.textBoxFileReadTime.TabIndex = 1;
+            // 
+            // textBoxFileReadCount
+            // 
+            this.textBoxFileReadCount.BackColor = System.Drawing.SystemColors.Info;
+            this.textBoxFileReadCount.Location = new System.Drawing.Point(427, 74);
+            this.textBoxFileReadCount.Name = "textBoxFileReadCount";
+            this.textBoxFileReadCount.Size = new System.Drawing.Size(207, 38);
+            this.textBoxFileReadCount.TabIndex = 2;
+            // 
+            // textBoxFind
+            // 
+            this.textBoxFind.Location = new System.Drawing.Point(427, 176);
+            this.textBoxFind.Name = "textBoxFind";
+            this.textBoxFind.Size = new System.Drawing.Size(207, 38);
+            this.textBoxFind.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(676, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "время чтения";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(682, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "уникальные слова";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "слово для поиска";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(289, 51);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Поиск (четкий)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBoxExactTime
+            // 
+            this.textBoxExactTime.BackColor = System.Drawing.SystemColors.Info;
+            this.textBoxExactTime.Location = new System.Drawing.Point(427, 336);
+            this.textBoxExactTime.Name = "textBoxExactTime";
+            this.textBoxExactTime.Size = new System.Drawing.Size(207, 38);
+            this.textBoxExactTime.TabIndex = 8;
+            // 
+            // textBoxMaxDist
+            // 
+            this.textBoxMaxDist.Location = new System.Drawing.Point(427, 512);
+            this.textBoxMaxDist.Name = "textBoxMaxDist";
+            this.textBoxMaxDist.Size = new System.Drawing.Size(207, 38);
+            this.textBoxMaxDist.TabIndex = 9;
+            // 
+            // textBoxThreadCount
+            // 
+            this.textBoxThreadCount.Location = new System.Drawing.Point(427, 618);
+            this.textBoxThreadCount.Name = "textBoxThreadCount";
+            this.textBoxThreadCount.Size = new System.Drawing.Size(207, 38);
+            this.textBoxThreadCount.TabIndex = 10;
+            // 
+            // textBoxThreadCountAll
+            // 
+            this.textBoxThreadCountAll.Location = new System.Drawing.Point(427, 713);
+            this.textBoxThreadCountAll.Name = "textBoxThreadCountAll";
+            this.textBoxThreadCountAll.Size = new System.Drawing.Size(207, 38);
+            this.textBoxThreadCountAll.TabIndex = 11;
+            // 
+            // textBoxApproxTime
+            // 
+            this.textBoxApproxTime.BackColor = System.Drawing.SystemColors.Info;
+            this.textBoxApproxTime.Location = new System.Drawing.Point(427, 815);
+            this.textBoxApproxTime.Name = "textBoxApproxTime";
+            this.textBoxApproxTime.Size = new System.Drawing.Size(207, 38);
+            this.textBoxApproxTime.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 512);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(289, 114);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Поиск (паралельный нечеткий)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 31;
+            this.listBoxResult.Location = new System.Drawing.Point(1072, 12);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(437, 841);
+            this.listBoxResult.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(640, 336);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(304, 32);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "время четкого поиска";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(714, 713);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(241, 32);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Макс расстояние";
+            //this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(725, 618);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(219, 32);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Кол-во потоков";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(665, 512);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(401, 32);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Вычесленное кол-во потоков";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(791, 815);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 32);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "время";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1569, 901);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBoxApproxTime);
+            this.Controls.Add(this.textBoxThreadCountAll);
+            this.Controls.Add(this.textBoxThreadCount);
+            this.Controls.Add(this.textBoxMaxDist);
+            this.Controls.Add(this.textBoxExactTime);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxFind);
+            this.Controls.Add(this.textBoxFileReadCount);
+            this.Controls.Add(this.textBoxFileReadTime);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxFileReadTime;
+        private System.Windows.Forms.TextBox textBoxFileReadCount;
+        private System.Windows.Forms.TextBox textBoxFind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxExactTime;
+        private System.Windows.Forms.TextBox textBoxMaxDist;
+        private System.Windows.Forms.TextBox textBoxThreadCount;
+        private System.Windows.Forms.TextBox textBoxThreadCountAll;
+        private System.Windows.Forms.TextBox textBoxApproxTime;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Lab 4-5/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        //{
+
+        //}
+        List<string> list = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "*|*.txt";
+            if (fd.ShowDialog() != DialogResult.OK)
+            {
+                MessageBox.Show(" невыбран файл");
+            }
+            else
+            {
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                //Чтение файла в виде строки
+                string text = File.ReadAllText(fd.FileName);
+                //Разделительные символы для чтения из файла 
+                char[] separators = new char[] {' ','.',',','!','?','/','\t','\n'};
+                string[] textArray = text.Split(separators);
+                foreach (string strTemp in textArray)
+                { //Удаление пробелов в начале и конце строки 
+                    string str = strTemp.Trim(); //Добавление строки в список, если строка не содержится в списке 
+                    if (!list.Contains(str))
+                        list.Add(str);
+                }
+                    t.Stop(); this.textBoxFileReadTime.Text = t.Elapsed.ToString();
+                    this.textBoxFileReadCount.Text = list.Count.ToString();
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string word = this.textBoxFind.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                string wordUpper = word.ToUpper();
+                List<string> tempList = new List<string>();
+                Stopwatch t = new Stopwatch(); t.Start();
+                foreach (string str in list)
+                {
+                    if (str.ToUpper().Contains(wordUpper))
+                    {
+                        tempList.Add(str);
+                    }
+                }
+                t.Stop();
+                this.textBoxExactTime.Text = t.Elapsed.ToString();
+                this.listBoxResult.BeginUpdate();
+                this.listBoxResult.Items.Clear();
+                foreach (string str in tempList)
+                {
+                    this.listBoxResult.Items.Add(str);
+                }
+                this.listBoxResult.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+        }
+
+
+        public class ParallelSearchResult
+        {
+            public string word { get; set; }
+            public int dist { get; set; }
+            public int ThreadNum { get; set; }
+        }
+        class ParallelSearchThreadParam
+        {
+            public List<string> tempList { get; set; }
+            public string wordPattern { get; set; }
+            public int maxDist { get; set; }
+            public int ThreadNum { get; set; }
+        }
+        public static List<ParallelSearchResult> ArrayThreadTask(object paramObj)
+        {
+            ParallelSearchThreadParam param = (ParallelSearchThreadParam)paramObj;
+            string wordUpper = param.wordPattern.Trim().ToUpper();
+            List<ParallelSearchResult> Result = new List<ParallelSearchResult>();
+            foreach (string str in param.tempList)
+            {
+                int dist = EditDistance.Distance(str.ToUpper(), wordUpper);
+                if (dist <= param.maxDist)
+                {
+                    ParallelSearchResult temp = new ParallelSearchResult()
+                    {
+                        word = str,
+                        dist = dist,
+                        ThreadNum = param.ThreadNum
+                    };
+                    Result.Add(temp);
+                }
+            }
+            return Result;
+        }
+        public class MinMax
+        {
+            public int Min { get; set; }
+            public int Max { get; set; }
+
+            public MinMax(int pmin, int pmax)
+            {
+                this.Min = pmin;
+                this.Max = pmax;
+            }
+        }
+        public static class SubArrays
+        {
+            public static List<MinMax> DivideSubArrays(int beginIndex, int endIndex, int subArraysCount)
+            {
+                List<MinMax> result = new List<MinMax>();
+                if ((endIndex - beginIndex) <= subArraysCount)
+                {
+                    result.Add(new MinMax(0, (endIndex - beginIndex)));
+                }
+                else
+                {
+                    int delta = (endIndex - beginIndex) / subArraysCount;
+                    int currentBegin = beginIndex;
+                    while ((endIndex - currentBegin) >= 2 * delta)
+                    {
+                        result.Add(new MinMax(currentBegin, currentBegin + delta));
+                        currentBegin += delta;
+                    }
+                    result.Add(new MinMax(currentBegin, endIndex));
+                }
+                return result;
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            string word = this.textBoxFind.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                int maxDist;
+                if (!int.TryParse(this.textBoxThreadCountAll.Text.Trim(), out maxDist))    //////////// !!!!!!!!!!!!!!!!!!!!
+                {
+                    MessageBox.Show("Необходимо указать максимальное расстояние");
+                    return;
+                }
+                if (maxDist < 1 || maxDist > 5)
+                {
+                    MessageBox.Show("Максимальное расстояние должно быть в диапазоне от 1 до 5");
+                    return;
+                }
+                int ThreadCount;
+                if (!int.TryParse(this.textBoxThreadCount.Text.Trim(), out ThreadCount))
+                {
+                    MessageBox.Show("Необходимо указать количество потоков");
+                    return;
+                }
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+                List<ParallelSearchResult> Result = new List<ParallelSearchResult>();
+                List<MinMax> arrayDivList = SubArrays.DivideSubArrays(0, list.Count, ThreadCount);
+                int count = arrayDivList.Count;
+                Task<List<ParallelSearchResult>>[] tasks = new Task<List<ParallelSearchResult>>[count];
+                for (int i = 0; i < count; i++)
+                {
+                    List<string> tempTaskList = list.GetRange(arrayDivList[i].Min, arrayDivList[i].Max - arrayDivList[i].Min);
+                    tasks[i] = new Task<List<ParallelSearchResult>>(ArrayThreadTask, new ParallelSearchThreadParam()
+                    {
+                        tempList = tempTaskList,
+                        maxDist = maxDist,
+                        ThreadNum = i,
+                        wordPattern = word
+                    });
+                    tasks[i].Start();
+                }
+                Task.WaitAll(tasks);
+                timer.Stop();
+                for (int i = 0; i < count; i++)
+                {
+                    Result.AddRange(tasks[i].Result);
+                }
+                timer.Stop();
+                this.textBoxApproxTime.Text = timer.Elapsed.ToString();
+                this.textBoxMaxDist.Text = count.ToString();
+                this.listBoxResult.BeginUpdate();
+                this.listBoxResult.Items.Clear();
+                foreach (var x in Result)
+                {
+                    string temp = x.word + "(расстояние=" + x.dist.ToString() + " поток=" + x.ThreadNum.ToString() + ")";
+                    this.listBoxResult.Items.Add(temp);
+                }
+                this.listBoxResult.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+        }
+
+    }
+    public static class EditDistance
+    {
+        public static int Distance(string str1Param, string str2Param)
+        {
+            if ((str1Param == null) || (str2Param == null)) return -1;
+            int str1Len = str1Param.Length; int str2Len = str2Param.Length;
+            if ((str1Len == 0) && (str2Len == 0)) return 0;
+            if (str1Len == 0) return str2Len;
+            if (str2Len == 0) return str1Len;
+            string str1 = str1Param.ToUpper();
+            string str2 = str2Param.ToUpper();
+            int[,] matrix = new int[str1Len + 1, str2Len + 1];
+            for (int i = 0; i <= str1Len; i++) matrix[i, 0] = i;
+            for (int j = 0; j <= str2Len; j++) matrix[0, j] = j;
+            for (int i = 1; i <= str1Len; i++)
+            {
+                for (int j = 1; j <= str2Len; j++)
+                {
+                    int symbEqual = ((str1.Substring(i - 1, 1) == str2.Substring(j - 1, 1)) ? 0 : 1);
+                    int ins = matrix[i, j - 1] + 1;
+                    int del = matrix[i - 1, j] + 1;
+                    int subst = matrix[i - 1, j - 1] + symbEqual;
+                    matrix[i, j] = Math.Min(Math.Min(ins, del), subst);
+                    if ((i > 1) && (j > 1) && (str1.Substring(i - 1, 1) == str2.Substring(j - 2, 1)) && (str1.Substring(i - 2, 1) == str2.Substring(j - 1, 1)))
+                    {
+                        matrix[i, j] = Math.Min(matrix[i, j], matrix[i - 2, j - 2] + symbEqual);
+                    }
+                }
+            }
+            return matrix[str1Len, str2Len];
+        }
+
+
+
+    }
+}
+
+
+
